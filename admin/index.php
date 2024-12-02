a5154638a128f76c4d0d7db407f03c982b468920
Aikarubi <nerupe@floridauniversitaria.es>
2024-12-02 12:21:00 +0100

    creacion de tabla para mostrar los datos

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,30 @@ incluirTemplate('header');
 
     <a href="../admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
     
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Casa en venta frente al bosque</td>
+                <td><img src="/bienes-raices-php/build/img/destacada.webp" class="imagen-tabla" alt="imagen casa"></td>
+                <td>$3.000.000</td>
+                <td>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
 </main>
 
 <?php
