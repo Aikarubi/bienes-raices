66b9b7ddae5874237d7fd9406f72ed802a0b6808
Aikarubi <nerupe@floridauniversitaria.es>
2024-12-02 19:48:34 +0100

    preparar la query y la conexion para mostrar los datos

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,19 @@
 <?php
 
+// Importar la conexión
+require '../includes/config/database.php';
+$db = conectarBD();
+
+// Escribir la query
+$query = "SELECT * FROM propiedades";
+
+// Consultar la BD
+$resultados = mysqli_query($db, $query);
+
+// Muestra mensaje condicional
 $mensaje = $_GET['mensaje'] ?? null; //Si no existe la variable se le da el valor de null
+
+// Incluye un template
 require '../includes/funciones.php';
 incluirTemplate('header');
 
@@ -9,12 +22,12 @@ incluirTemplate('header');
 
 <main class="contenedor seccion">
     <h1>Administrador de Bienes Raices</h1>
-    <?php if( intval($mensaje) === 1): ?>
+    <?php if (intval($mensaje) === 1): ?>
         <p class="alerta exito">Anuncio Creado Correctamente</p>
-    <?php endif; ?> 
+    <?php endif; ?>
 
     <a href="../admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
-    
+
     <table class="propiedades">
         <thead>
             <tr>
@@ -25,7 +38,7 @@ incluirTemplate('header');
                 <th>Acciones</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody> <!-- Mostrar los resultados -->
             <tr>
                 <td>1</td>
                 <td>Casa en venta frente al bosque</td>
@@ -42,6 +55,8 @@ incluirTemplate('header');
 </main>
 
 <?php
+
+// Cerrar la conexión
 
 incluirTemplate('footer');
 
