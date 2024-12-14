f9aa4206099def918ee33677ea6203454336c143
Aikarubi <nerupe@floridauniversitaria.es>
2024-12-14 18:50:18 +0100

    mostrar todos los anuncios y redirigir a su anuncio propio

--- includes/templates/anuncios.php
+++ includes/templates/anuncios.php
@@ -14,35 +14,32 @@ $resultado = mysqli_query($db, $query);
 <div class="contenedor-anuncios">
     <?php while ($propiedad = mysqli_fetch_assoc($resultado)): ?>
         <div class="anuncio">
-            <picture>
-                <source srcset="/bienes-raices-php/build/img/anuncio1.webp" type="image/webp">
-                <source src="/bienes-raices-php/build/img/anuncio1.jpg" type="image/jpeg">
-                <img loading="lazy" src="/bienes-raices-php/build/img/anuncio1.jpg" alt="Imagen Anuncio">
-            </picture>
+
+            <img loading="lazy" src="/bienes-raices-php/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen Anuncio">
 
             <div class="contenido-anuncio">
-                <h3>Casa de Lujo en el Lago</h3>
-                <p>Casa en el lago con una vista increíble, acabados de lujo y a excelente precio.</p>
-                <p class="precio">$3.000.000</p>
+                <h3><?php echo $propiedad['titulo']; ?></h3>
+                <p><?php echo $propiedad['descripcion']; ?></p>
+                <p class="precio"> $<?php echo $propiedad['precio']; ?></p>
 
                 <ul class="iconos-caracteristicas">
                     <li>
                         <img class="icono-departamento" loading="lazy" src="/bienes-raices-php/build/img/wc.svg" alt="icono wc">
-                        <p>3</p>
+                        <p><?php echo $propiedad['wc']; ?></p>
                     </li>
                     <li>
                         <img class="icono-departamento" loading="lazy" src="/bienes-raices-php/build/img/parking.svg"
                             alt="icono estacionamiento">
-                        <p>3</p>
+                        <p><?php echo $propiedad['estacionamiento']; ?></p>
                     </li>
                     <li>
                         <img class="icono-departamento" loading="lazy" src="/bienes-raices-php/build/img/bed.svg"
                             alt="icono habitaciones">
-                        <p>4</p>
+                        <p><?php echo $propiedad['habitaciones']; ?></p>
                     </li>
                 </ul>
 
-                <a href="anuncio.html" class="boton-amarillo-block">Ver Propiedad</a>
+                <a href="anuncio.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Ver Propiedad</a>
             </div><!--.contenido-anuncio-->
         </div><!--.anuncio-->
     <?php endwhile; ?>
