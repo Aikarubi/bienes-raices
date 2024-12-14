f1caec868f549c7c88f1ea6c09b991634498c0d7
Aikarubi <nerupe@floridauniversitaria.es>
2024-12-14 18:55:29 +0100

    descripcion completa de la propiedad

--- anuncio.php
+++ anuncio.php
@@ -1,42 +1,52 @@
 <?php
+//Importar la conexion de la BD
+require 'includes/config/database.php';
+$db = conectarBD();
+
+//Consultar
+$query = "SELECT * FROM propiedades WHERE id = '$_GET[id]'";
+
+//Obtener resultados
+$resultado = mysqli_query($db, $query);
 
 require 'includes/funciones.php';
 incluirTemplate('header');
-
 ?>
 
 
 <main class="contenedor seccion contenido-centrado">
-    <h1>Casa en Venta frente al bosque</h1>
+<?php while ($propiedad = mysqli_fetch_assoc($resultado)): ?>
+    <h1><?php echo $propiedad['titulo']; ?></h1>
 
-    <picture>
-        <source srcset="/bienes-raices-php/build/img/destacada.webp" type="image/webp">
-        <source srcset="/bienes-raices-php/build/img/destacada.jpg" type="image/jpeg">
-        <img lazy="loading" src="/bienes-raices-php/build/img/destacada.jpg" alt="Casa en venta">
-    </picture>
+        <img lazy="loading" src="/bienes-raices-php/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Casa en venta">
 
     <div class="resumen-propiedad">
-        <p class="precio">$3.000.000</p>
+        <p class="precio">$<?php echo $propiedad['precio']; ?></p>
         <ul class="iconos-caracteristicas">
             <li>
                 <img class="icono-departamento" loading="lazy" src="/bienes-raices-php/build/img/wc.svg" alt="icono wc">
-                <p>5</p>
+                <p><?php echo $propiedad['wc']; ?></p>
             </li>
             <li>
                 <img class="icono-departamento" loading="lazy" src="/bienes-raices-php/build/img/parking.svg" alt="icono estacionamiento">
-                <p>4</p>
+                <p><?php echo $propiedad['estacionamiento']; ?></p>
             </li>
             <li>
                 <img class="icono-departamento" loading="lazy" src="/bienes-raices-php/build/img/bed.svg" alt="icono habitaciones">
-                <p>8</p>
+                <p><?php echo $propiedad['habitaciones']; ?></p>
             </li>
         </ul>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In corrupti provident voluptatem nesciunt culpa tempora, perspiciatis autem eos. Est aliquam consequuntur quia consequatur, placeat tempore? Neque natus hic inventore atque?</p>
+        <p><?php echo $propiedad['descripcion']; ?></p>
     </div>
+
+<?php endwhile; ?>
 </main>
 
 <?php
 
 incluirTemplate('footer');
+
+//Cerrar la conexion de la BD
+mysqli_close($db);
 
 ?>
