e3fef8d7a66e00cbc841e0dd1c3838bcf4fb5275
Aikarubi <nerupe@floridauniversitaria.es>
2024-12-19 12:42:10 +0100

    creando formulario de login

--- login.php
+++ login.php
@@ -6,8 +6,22 @@ incluirTemplate('header');
 ?>
 
 
-<main class="contenedor seccion">
+<main class="contenedor seccion contenido-centrado">
     <h1>Iniciar Sesión</h1>
+
+    <form class="formulario">
+    <fieldset>
+            <legend>Email y Contraseña</legend>
+
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" placeholder="Tu E-mail" required>
+
+            <label for="contrasenya">Contraseña:</label>
+            <input type="password" id="contrasenya" name="contrasenya" placeholder="Tu Contraseña">
+
+        </fieldset>
+        <input type="submit" value="Iniciar Sesión" class="boton boton-azul">
+    </form>
 </main>
 
 <?php
