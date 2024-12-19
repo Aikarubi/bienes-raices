e995a735e69eea4e2d92a0b80adf268284c7612c
Aikarubi <nerupe@floridauniversitaria.es>
2024-12-19 12:25:09 +0100

    archivo de login creado

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'includes/funciones.php';
+incluirTemplate('header');
+
+?>
+
+
+<main class="contenedor seccion">
+    <h1>Iniciar Sesión</h1>
+</main>
+
+<?php
+
+incluirTemplate('footer');
+
+?>
