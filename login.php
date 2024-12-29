4c8eeb4531896ecd74551d478bbb614115e94a3e
Aikarubi <nerupe@floridauniversitaria.es>
2024-12-29 12:20:21 +0100

    validacion de credenciales

--- login.php
+++ login.php
@@ -1,5 +1,23 @@
 <?php
 
+//CONECTAR A LA BD
+require 'includes/config/database.php';
+$db = conectarBD();
+
+//AUTENTICAR USUARIO
+$errores = [];
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+     $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+     $contrasenya = mysqli_real_escape_string($db, $_POST['contrasenya']);
+
+     if(!$email) { $errores[] = 'El E-mail es obligatorio'; }
+
+     if(!$contrasenya) { $errores[] = 'La Contraseña es obligatoria'; }
+
+     if(empty($errores)) { $errores[] = 'E-mail o Contraseña Incorrectos'; }
+}
+
 require 'includes/funciones.php';
 incluirTemplate('header');
 
@@ -9,12 +27,18 @@ incluirTemplate('header');
 <main class="contenedor seccion contenido-centrado">
     <h1>Iniciar Sesión</h1>
 
-    <form class="formulario">
+    <?php foreach($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+
+    <form method="$_POST" class="formulario">
     <fieldset>
             <legend>Email y Contraseña</legend>
 
             <label for="email">E-mail:</label>
-            <input type="email" id="email" name="email" placeholder="Tu E-mail" required>
+            <input type="email" id="email" name="email" placeholder="Tu E-mail">
 
             <label for="contrasenya">Contraseña:</label>
             <input type="password" id="contrasenya" name="contrasenya" placeholder="Tu Contraseña">
